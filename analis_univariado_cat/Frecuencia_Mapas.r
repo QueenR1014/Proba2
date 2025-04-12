3861818404a1ad7571c8502ba47a462b94338e69
DanielM <54962223+DanielM13031@users.noreply.github.com>
2025-04-12 16:18:47 -0500

    moviminetos y creacion de carpeta para mis graficos

--- analis_univariado_cat/Frecuencia_Mapas.r
+++ analis_univariado_cat/Frecuencia_Mapas.r
@@ -5,8 +5,11 @@ pick_map <- subset(draft, Tournament == "Valorant Champions 2024" & Action != "b
 
 conteo_mapas <- table(pick_map$Map)
 
+pdf("graficos/frecuencia_mapas.pdf", width = 8, height = 6)
 barplot(conteo_mapas,
         main = "Frecuencia de Mapas (Valorant Champions 2024)",
         xlab = "Mapa",
         ylab = "Frecuencia",
         col = "#9c1a87")
+
+dev.off()
